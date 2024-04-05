5cf61428e07cfab949490557a1c32db6cb69a43d
kyung suk <dno06117@naver.com>
2024-04-05 21:29:02 +0900

    p198 Order, OrderItem 엔티티 설계 및 양방향 다대일 매핑

---
+++ src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,39 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders") // 정렬시 사용되는 order이라는 키워드 때문에 orders로 변경
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member; // 한명의 회원은 여러번 주문 가능 -> 주문 엔티티 기준 다대일 매핑
+
+    private LocalDateTime orderDate; // 주문일
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문 상태
+
+    // OrderItem 생성 후 Order에 추가 (양방향 매핑이 된다~~
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
---
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,33 @@
+package com.shop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_item_id")
+    private Item item; // 하나의 상품은 여러 주문 상품으로 들어갈 수 있음. 따라서 주문 상품 기준 many to one
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order; // 하나의 주문에 여러개의 상품 가능. 따라서 주문 상품 기준 주문을 many to one
+    // manytoone이랑 onetomany는 앞에 있는게 쓰여진곳 기준이라 생각하면 된다.
+    // 즉, manytoone이면 현재 엔티티가 many라는 뜻(현재 엔티티가 여러개 있을 수 있다는 뜻)
+
+    private int orderPrice; // 주문 가격
+    private int count; // 수량
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
