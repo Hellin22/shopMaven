5ebe21409bcce6aaf1deec2c3128deff32951ec1
kyeongSeok <dno06117@naver.com>
2024-04-10 23:33:59 +0900

    p280 main에서 사용자에게 보여줄 MainItemDto 객체 생성. @QueryProjection 사용

---
+++ src/main/java/com/shop/dto/MainItemDto.java
@@ -0,0 +1,28 @@
+package com.shop.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MainItemDto {
+    private Long id;
+
+    private String itemNm;
+
+    private String itemDetail;
+
+    private String imgUrl;
+
+    private Integer price;
+
+    @QueryProjection
+    public MainItemDto(Long id, String itemNm, String itemDetail, String imgUrl, Integer price) {
+        this.id = id;
+        this.itemNm = itemNm;
+        this.itemDetail = itemDetail;
+        this.imgUrl = imgUrl;
+        this.price = price;
+    } // @QueryProjection 어노테이션을 통해 querydsl로 결과 조회시 MainItemDto 객체로 받아오도록 한다.
+}
